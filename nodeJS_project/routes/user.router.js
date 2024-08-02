d63ab4c94565359923e1141f96fe1eaf77851b7b
VietNamCombatZ <127002473+YoshiGamingNoYT@users.noreply.github.com>
2024-08-02 20:06:15 +0700

    new docker

--- nodeJS_project/routes/user.router.js
+++ nodeJS_project/routes/user.router.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const userController = require("../controller/user.controller.js");
 const middleware = require("../middleware/middleware.js");
 
-
-
+router.get("/welcome", async (req, res) => {
+  res.status(200).send("Welcome to the app =)))");
+});
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
@@ -14,19 +15,39 @@ router.get("/getAllUsers", userController.getAllUsers);
 router.post("/forgot-pass", userController.forgotPass);
 router.post("/reset-pass", userController.resetPass);
 
-router.post("/single", middleware.uploadSingleFile,userController.uploadSingleFile); 
-router.post("/multiple",middleware.uploadMultipleFile, userController.uploadMultipleFile);
+router.post(
+  "/single",
+  middleware.uploadSingleFile,
+  userController.uploadSingleFile
+);
+router.post(
+  "/multiple",
+  middleware.uploadMultipleFile,
+  userController.uploadMultipleFile
+);
 
-router.post("/check", middleware.validateToken, async(req,res) => {
-    res.status(200).send("check");
-})
+router.post("/check", middleware.validateToken, async (req, res) => {
+  res.status(200).send("check");
+});
 
-
-router.post("/create-poll", middleware.validateToken , middleware.createPoll,userController.createPoll);
-router.delete("/delete-poll/:id", middleware.deletePoll,userController.deletePoll);
-router.post('/create-option', middleware.createOption, userController.createOption)
-router.post("/vote",middleware.vote, userController.vote);
-router.delete("/unvote",middleware.unVote, userController.unVote);
-router.get("/get-vote/:id",middleware.getVote, userController.getVote);
+router.post(
+  "/create-poll",
+  middleware.validateToken,
+  middleware.createPoll,
+  userController.createPoll
+);
+router.delete(
+  "/delete-poll/:id",
+  middleware.deletePoll,
+  userController.deletePoll
+);
+router.post(
+  "/create-option",
+  middleware.createOption,
+  userController.createOption
+);
+router.post("/vote", middleware.vote, userController.vote);
+router.delete("/unvote", middleware.unVote, userController.unVote);
+router.get("/get-vote/:id", middleware.getVote, userController.getVote);
 
 module.exports = router;
